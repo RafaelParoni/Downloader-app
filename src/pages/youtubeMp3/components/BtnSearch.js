9b7198031296bef4805b4e2f5882168e8f5797ce
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-05-15 23:10:27 -0300

    Update BtnSearch.js

--- src/pages/youtubeMp3/components/BtnSearch.js
+++ src/pages/youtubeMp3/components/BtnSearch.js
@@ -107,7 +107,7 @@ function HomeSearch() {
    
         }else{
           Mp4Info = {
-            VideoQuality: response.data.formats[2].url,
+            VideoQuality: 'dadadada',
             VideoUrl: response.data.formats[2].url,
           }
           videoInfo = {
